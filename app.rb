dfe7b1238666d98e7106e028a86231bbc8b6801a
Jason Lane & Justin Scott <jason_and_justin@fake.com>
2015-08-05 15:12:10 -0700

    Player one can play robot

--- app.rb
+++ app.rb
@@ -19,5 +19,30 @@ get '/game' do
 end
 
 get '/robot' do
+
   erb(:robot)
+end
+
+get '/robot_game' do
+  robot = rand(3)+1
+  if robot == 1
+    robot = 'rock'
+  elsif robot == 2
+    robot = 'scissors'
+  elsif robot == 3
+    robot = 'paper'
+  end
+  @game_boolean = params['player_one'].beats?(robot)
+  if @game_boolean == true
+    @winner = 'Player one is the winner!'
+    @user_input = params['player_one']
+    @robot_input = robot
+  elsif @game_boolean == false
+    @winner = 'Robot is the winner!'
+    @user_input = params['player_one']
+    @robot_input = robot
+  else
+    @winner = "<a href='/robot'>Try again</a>"
+  end
+  erb :robot_game
 end
