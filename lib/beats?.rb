3251e0ff2d8f8679d4202b50fc1f670f1397cb22
Jason Lane & Justin Scott <jason_and_justin@fake.com>
2015-08-05 12:01:23 -0700

    Add scissors loses to rock

--- lib/beats?.rb
+++ lib/beats?.rb
@@ -2,6 +2,8 @@ class String
   define_method(:beats?) do |other|
     if self == "rock" && other == "scissors"
       true
+    elsif self == "scissors" && other == "rock"
+      false
     end
   end
 end
