ecca3544320e966e5fe62f3deb689dda7d91dbbf
Jason Lane & Justin Scott <jason_and_justin@fake.com>
2015-08-05 12:03:44 -0700

    Add test rock doesnt beat paper

--- spec/rock_paper_spec.rb
+++ spec/rock_paper_spec.rb
@@ -9,4 +9,8 @@ describe('String#beats?') do
   it('returns false if scissors is an object and rock is the argument') do
     expect("scissors".beats?('rock')).to(eq(false))
   end
+
+  it('returns false if rock is an object and paper is the argument') do
+    expect("rock".beats?("paper")).to(eq(false))
+  end
 end
